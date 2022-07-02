b556713b4464725ccd359156fb19ea0318143566
ADESSO\mahmad <mugheera.ahmad@adesso.de>
2022-07-02 19:14:46 +0200

    Added the mu-plugins folder

---
+++ mu-plugins/university-post-types.php
@@ -0,0 +1,24 @@
+<!-- Add the "mu-plugins" folder to the "wp-content" folder of the site. Don't change the folder name from "mu-plugins".
+This file is used to add new Post Types to the site. The "mu-plugins" should only reside in the "wp-content"´folder. -->
+
+<?php 
+
+    // WordPress comes with two post types, 'Pages' and 'Post'. This method is used to setup new post types
+    function university_post_types() {
+        register_post_type('event', array(
+            'public' => true,
+            'show_in_rest' => true,
+            'labels' => array(
+                'name' => 'Events',
+                'add_new_item' => 'Add New Event',
+                'edit_item' => 'Edit Event',
+                'all_items' => 'All Events',
+                'singular_name' => 'Event'
+            ),
+            'menu_icon' => 'dashicons-calendar'
+        ));
+    }
+
+    add_action('init', 'university_post_types');
+
+?>
